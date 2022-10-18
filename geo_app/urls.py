faf4eb9d47acd3e6ea34f86b61757ae4f0c25fa2
tsyganno <tsyganlee@gmail.com>
2022-10-18 21:48:46 +0800

    fourth

--- geo_app/urls.py
+++ geo_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from geo_app.views import IndexView, PrivateRoomView
+from geo_app.views import IndexView, PrivateRoomView, ParsingCitiesView, process_parsing, EndingParsingView
 
 app_name = 'geo'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('private_room/', PrivateRoomView.as_view(), name='private_room'),
+    path('private_room/parsing_cities/', ParsingCitiesView.as_view(), name='parsing_cities'),
+    path('private_room/parsing_cities/process_parsing', process_parsing, name='process_parsing'),
+    path('private_room/parsing_cities/ending_parsing', EndingParsingView.as_view(), name='ending_parsing'),
 ]
--- geo_app/views.py
+++ geo_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import reverse
+from django.http import HttpResponseNotFound, HttpResponseServerError, HttpResponseRedirect
 
 
 class IndexView(TemplateView):
@@ -11,3 +13,16 @@ class PrivateRoomView(LoginRequiredMixin, TemplateView):
     login_url = 'accounts:login'
     template_name = 'geo_app/private_room.html'
 
+
+class ParsingCitiesView(LoginRequiredMixin, TemplateView):
+    login_url = 'accounts:login'
+    template_name = 'geo_app/parsing_cities.html'
+
+
+def process_parsing(request):
+    return HttpResponseRedirect(reverse('geo:ending_parsing'))
+
+
+class EndingParsingView(LoginRequiredMixin, TemplateView):
+    login_url = 'accounts:login'
+    template_name = 'geo_app/ending_parsing.html'
