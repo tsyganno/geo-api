3f5d0d3791e5e21ed31d30096dac759bdf7304db
tsyganno <tsyganlee@gmail.com>
2022-10-18 22:20:32 +0800

    add dadata

--- geo_app/models.py
+++ geo_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+# class City(models.Model):
+#     address = models.CharField(max_length=50, verbose_name='Адрес')
+#     postal_code = models.IntegerField(max_length=10, verbose_name='Почтовый Код')
+#     country = models.CharField(max_length=50, verbose_name='Страна')
+#     federal_district = models.CharField(max_length=50, verbose_name='Федеральный округ')
+#     region_type = models.CharField(max_length=50, verbose_name='Тип региона')
+#     region = models.CharField(max_length=50, verbose_name='Область')
+#     area_type = models.CharField(max_length=50, verbose_name='Тип области')
+#     area = models.CharField(max_length=50, verbose_name='Область')
+#     city_type = models.CharField(max_length=50, verbose_name='Тип города')
+#     city = models.CharField(max_length=20, verbose_name='Город')
+#     settlement_type = models.CharField(max_length=50, verbose_name='Тип поселения')
+#     settlement = models.CharField(max_length=50, verbose_name='Поселок')
+#     kladr_id = models.CharField(max_length=50, verbose_name='Идентификатор кладра')
+#
+#     def __str__(self):
+#         return self.city
+#
+#     class Meta:
+#         verbose_name_plural = 'Опросы'
+#         verbose_name = 'Опрос'
+#         ordering = ['-published']
--- geo_app/views.py
+++ geo_app/views.py
@@ -1,3 +1,5 @@
+from dadata import Dadata
+
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -20,6 +22,13 @@ class ParsingCitiesView(LoginRequiredMixin, TemplateView):
 
 
 def process_parsing(request):
+    token = "180d4258a7350e2d2e7aa62d1c31d1f57d24575a"
+    secret = "18ac391b58a8c17b578a20e753c1b79d1926915f"
+    dadata = Dadata(token, secret)
+    result = dadata.clean("address", "Алтайский край, г Белокуриха,659900,Россия,Сибирский,край,Алтайский,,,город,Белокуриха,,,2200000300000,e4edca96-9b86-4cac-8c7f-cc93d9ba4cd1,4,0,1404000000,1704000001,2204,UTC+7,51.996152,84.9839604,15072,1846")
+    for key in result.keys():
+        print(key, result[key])
+
     return HttpResponseRedirect(reverse('geo:ending_parsing'))
 
 
