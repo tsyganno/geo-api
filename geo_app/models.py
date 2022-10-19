3b501b435eeee1cbbd979e9294d5b8550f0dbd74
tsyganno <tsyganlee@gmail.com>
2022-10-19 22:43:33 +0800

    kek

--- geo_app/models.py
+++ geo_app/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class City(models.Model):
-    country = models.CharField(max_length=50, verbose_name='Страна')
-    federal_district = models.CharField(max_length=50, verbose_name='Федеральный округ')
-    region = models.CharField(max_length=50, verbose_name='Область')
-    city = models.CharField(max_length=20, verbose_name='Город')
-    street = models.CharField(max_length=50, verbose_name='Улица')
-    house = models.CharField(max_length=50, verbose_name='Номер дома')
-    block = models.CharField(max_length=50, verbose_name='Блок дома')
-    flat = models.CharField(max_length=50, verbose_name='Номер квартиры')
-    geo_lat = models.FloatField(verbose_name='Географическая широта')
-    geo_lon = models.FloatField(verbose_name='Географическая долгота')
+    country = models.CharField(null=True, max_length=50, verbose_name='Страна')
+    federal_district = models.CharField(null=True, max_length=50, verbose_name='Федеральный округ')
+    region = models.CharField(null=True, max_length=50, verbose_name='Область')
+    city = models.CharField(null=True, max_length=20, verbose_name='Город')
+    street = models.CharField(null=True, max_length=50, verbose_name='Улица')
+    house = models.CharField(null=True, max_length=50, verbose_name='Номер дома')
+    block = models.CharField(null=True, max_length=50, verbose_name='Блок дома')
+    flat = models.CharField(null=True, max_length=50, verbose_name='Номер квартиры')
+    geo_lat = models.CharField(null=True, max_length=50, verbose_name='Географическая широта')
+    geo_lon = models.CharField(null=True, max_length=50, verbose_name='Географическая долгота')
 
     def __str__(self):
         return self.city
--- geo_app/views.py
+++ geo_app/views.py
@@ -5,6 +5,7 @@ from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import reverse
 from django.http import HttpResponseNotFound, HttpResponseServerError, HttpResponseRedirect
+from geo_app.models import City
 
 
 class IndexView(TemplateView):
@@ -22,13 +23,29 @@ class ParsingCitiesView(LoginRequiredMixin, TemplateView):
 
 
 def process_parsing(request):
+    count = 0
+    city_element = City()
     token = "180d4258a7350e2d2e7aa62d1c31d1f57d24575a"
     secret = "18ac391b58a8c17b578a20e753c1b79d1926915f"
     dadata = Dadata(token, secret)
-    result = dadata.clean("address", "Алтайский край, г Белокуриха,659900,Россия,Сибирский,край,Алтайский,,,город,Белокуриха,,,2200000300000,e4edca96-9b86-4cac-8c7f-cc93d9ba4cd1,4,0,1404000000,1704000001,2204,UTC+7,51.996152,84.9839604,15072,1846")
-    for key in result.keys():
-        print(key, result[key])
-
+    with open('cities/city.csv', 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line.split(','))
+            print()
+            # result = dadata.clean("address", line.strip())
+            # city_element.country = result['country']
+            # city_element.federal_district = result['federal_district']
+            # city_element.region = result['region']
+            # city_element.city = result['city']
+            # city_element.street = result['street']
+            # city_element.house = result['house']
+            # city_element.block = result['block']
+            # city_element.flat = result['flat']
+            # city_element.geo_lat = result['geo_lat']
+            # city_element.geo_lon = result['geo_lon']
+            # city_element.save()
+            count += 1
+    print(count)
     return HttpResponseRedirect(reverse('geo:ending_parsing'))
 
 
